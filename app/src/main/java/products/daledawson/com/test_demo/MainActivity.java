dad3c4fb5e1b541ff9915f8915f5901c2c73681f
zhengquan <zhengquan183@163.com>
2017-12-07 14:44:27 +0800

    first commit

---
+++ app/src/main/java/products/daledawson/com/test_demo/MainActivity.java
@@ -0,0 +1,13 @@
+package products.daledawson.com.test_demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
